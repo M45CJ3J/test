5a1caf0842280dd26f19e35a9a9503ce54c8013e
AbdallahIbrahim <55169846+M45CJ3J@users.noreply.github.com>
2024-11-23 10:18:00 +0200

    Update server.py

--- server.py
+++ server.py
@@ -41,5 +41,5 @@ def index():
         return render_template('index.html')
 
 
-if __name__=="__main__":
-    app.run("0.0.0.0")
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0", port=5000)
